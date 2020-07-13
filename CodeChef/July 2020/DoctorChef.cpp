1ffdfbbe70d75d00ac8b0c6bace8ca59f26f839e
lonewolf <saurabhdalakoti07@gmail.com>
2020-07-13 18:14:51 +0530

    two questions up solved

---
+++ CodeChef/July 2020/DoctorChef.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define FASTIO ios_base::sync_with_stdio(NULL); cin.tie(NULL);
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define pb push_back
+
+using namespace std;
+
+/*
+    What the hell went wrong
+    unable to generalise the idea and not experimenting
+    Practise greedy
+*/
+
+
+void init(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin) ;
+  freopen("output.txt", "w", stdout) ;
+  #endif
+}
+
+int solve(){
+    int n,x;
+    cin>>n>>x;
+    vector<int> countries(n);
+    for(int i=0;i<n;i++)
+        cin>>countries[i];
+    sort(countries.begin(),countries.end());
+    auto itr=lower_bound(countries.begin(),countries.end(),x);
+    int startIdx=itr-countries.begin();
+    int days=0,caseOne=0;
+    // checking what would be answer if we start from this country and serve the greater countries ahead
+    for(int i=startIdx;i<n;i++){
+        if(x<countries[i]){
+            while(x<countries[i]){
+                x*=2;
+                days++;
+            }
+            days++;
+        }else{
+            days++;
+        }
+        x=2*countries[i];
+    }
+    caseOne= days+startIdx ;// because we are curring big companies first and then next big companies and when all this is done
+    // we can cure small countries in one day
+
+    // covering a edge case that given 4 countries 30,40,80 and 180 and k=60 start with lowerbound would give 6
+    // but starting from a element before lowerbound would give better answer which is 5
+    days=0;
+    int caseTwo=0;
+    if(startIdx!=0){
+        startIdx--;
+        for(int i=startIdx;i<n;i++){
+            if(x<countries[i]){
+                while(x<countries[i]){
+                    x*=2;
+                    days++;
+                }
+                days++;
+            }else{
+                days++;
+            }
+            x=2*countries[i];
+        }
+        caseTwo=days+startIdx;
+        cout<<min(caseTwo,caseOne)<<endl;
+    }else{
+        cout<<caseOne<<endl;
+    }
+
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
--- CodeChef/July 2020/chefAndSwap.cpp
+++ CodeChef/July 2020/chefAndSwap.cpp
@@ -1,4 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define FASTIO ios_base::sync_with_stdio(NULL); cin.tie(NULL);
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define pb push_back
+
 using namespace std;
 
 /*
@@ -6,83 +15,85 @@ using namespace std;
     codes become 2 time slower when using long long instead of int
     Don't underestimate greedy
     map[key] return 0 if key is absent dont check if count ==0 and then 
+    No need to make array ll unnecessarily as it can increase time execution and promote TLE
 */
 
-long long go(map<int,int>& commonElements,map<int,int>& elementsInA,map<int,int>& elementsInB,int minINBoth){
-    for(auto m:commonElements){
-        if(m.second%2)
-            return -1;
-    }
-    long long ans=0;
-    // see elemens in A
-    vector<int> extraElementsWithA,extraElementsWithB;
-    for(auto m:elementsInA){
-        int x=elementsInB[m.first];
-        if(x<m.second){
-            for(int i=0;i<(m.second-x)/2;i++)
-                extraElementsWithA.push_back(m.first);
-        }else{
-            for(int i=0;i<(x-m.second)/2;i++)
-                extraElementsWithB.push_back(m.first);
-        }
-    }
+// https://discuss.codechef.com/t/chfnswps-editorial/71067
 
-    for(auto m:elementsInB){
-        if(elementsInA.count(m.first)==0){
-            for(int i=0;i<m.second/2;i++)
-                extraElementsWithB.push_back(m.first);
-        }
-    }
+void init(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin) ;
+  freopen("output.txt", "w", stdout) ;
+  #endif
+}
 
-    sort(extraElementsWithA.begin(),extraElementsWithA.end());
-    sort(extraElementsWithB.begin(),extraElementsWithB.end());
-    ans=0;
-    long long l=0;
-    long long n=extraElementsWithA.size();
-    vector<int> results;
+// print vector
+// print vector of pair int
+// print hashmap
+
+void solve(){
+    int n,x;
+    cin>>n;
+    vector<int> vec1(n),vec2(n);
+    map<int,int> mappy;
     for(int i=0;i<n;i++){
-        if(extraElementsWithA[i]<2*minINBoth)
-            results.push_back(extraElementsWithA[i]);
-        if(extraElementsWithB[i]<2*minINBoth)
-            results.push_back(extraElementsWithB[i]);
+        cin>>x;
+        mappy[x]++;
+        vec1[i]=x;
     }
-    sort(results.begin(),results.end());
-    int x=results.size();
-    if(x>=n){
-        for(int i=0;i<n;i++)
-        ans+=(long long)results[i];
-        return ans;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        mappy[x]--;
+        vec2[i]=x;
     }
-    for(int i=0;i<x;i++){
-        ans+=(long long)results[i];
+    // now if mapp[i] is >0 means vec1 had that element in acces or vice versa
+    vec1.clear();
+    vec2.clear();
+    int minimum=INT_MAX;
+    bool isPossible=true;
+    for(auto m: mappy){
+        minimum=min(m.first,minimum);
+        if(abs(m.second)%2){
+            isPossible=false;
+            break;
+        }
+        if(m.second>0){
+            int s=(m.second)/2;
+            while(s--)
+                vec1.push_back(m.first);
+        }else{
+            int s=(abs(m.second))/2;
+            while(s--)
+                vec2.push_back(m.first);
+        }
     }
-    l=2*n-2*x;
-    ans+=l*minINBoth;
-    return ans;
+    if(!isPossible){
+        cout<<"-1\n";
+        return;
+    }
+    // calculation time
+    // and its sure that vec2 and vec1 are equal in size
+    ll cost=0;
+    // no need to do sorting as elements were sorted as per key in map stl !
+    // sort(vec1.begin(),vec2.end());
+    // sort(vec2.begin(),vec2.end());
+    // as discussed we had two types of swaps, one with the help of min elem and other min(arr1[i],arr1[i])
+    // now we dont want to fight with min(min of vec1, min of vec2 ), thus we would be greedy and would reverse one array
+    reverse(vec2.begin(),vec2.end());
+    for(int i=0;i<vec1.size();i++)
+        cost+=min((ll)2*minimum,min((ll)vec1[i],(ll)vec2[i]));
+    cout<<cost<<endl;
+    // this was hell 
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    FASTIO;
     int t;
     cin>>t;
     while(t--){
-        int n,x,minINBoth=INT_MAX;
-        cin>>n;
-        map<int,int> commonElements, elementsInA, elementsInB;
-        for(int i=0;i<n;i++){
-            cin>>x;
-            commonElements[x]+=1;
-            elementsInA[x]+=1;
-            minINBoth=min(minINBoth,x);
-        }
-        for(int i=0;i<n;i++){
-            cin>>x;
-            commonElements[x]+=1;
-            elementsInB[x]+=1;
-            minINBoth=min(minINBoth,x);
-        }
-
-        cout<<go(commonElements,elementsInA,elementsInB,minINBoth)<<"\n";
+        solve();
     }
 }
