577139696dfb0ce2fa362a8d4416a830bf7e7990
lonewolf <saurabhdalakoti07@gmail.com>
2020-12-04 14:37:21 +0530

    GFG rotate image by 90 degrees

---
+++ geek for geeks/90DegreeRotate.cpp
@@ -0,0 +1,21 @@
+void rotate(vector<vector<int>>& matrix) {
+    int N = matrix.size();
+    if(N==0)
+        return;
+    
+    // Transpose of Matrix , it is inplace.
+    for(int i=0; i<N; i++) 
+    {
+        for(int j=0; j<i; j++) 
+        {
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    // Reverse the Matrix Row-wise
+    for(int i=0; i<N; i++) 
+    {
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+    
+    return;
+}
